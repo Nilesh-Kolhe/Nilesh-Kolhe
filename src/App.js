0510912e1f8ca8d34bbd3be085e3116d313305cc
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-08-14 20:28:30 +0530

    Copied from Other Branch

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import image from "./img2.jpg"; 
+
+function App() {
+
+  return (
+    <div className="App" style={{ display: "flex", flexDirection: "column", backgroundImage:`url(${image})`, backgroundSize:"cover" }}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/education' element={<Education />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router >
+    </div >
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.js";
+import './NavBar.css';
+import image from './logo-svg.svg'
+
+const NavBar = () => {
+
+    return (
+        <div style={{ height: "50px" }}>
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-black" href="/">
+                        <img src={`${image}`} alt="Kiwi standing on oval" />
+                    </a>
+                    <button className="navbar-toggler collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbar"
+                        aria-controls="navbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="navbar-collapse collapse" id="navbar">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-lg-end" style={{ width: "100%" }}>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/education">Education</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/experience">Experience</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contact">Contact Me</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="/">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
