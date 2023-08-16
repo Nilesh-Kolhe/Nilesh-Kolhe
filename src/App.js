01ca4e197b0d9e3d334d1904d910a568861a6383
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-08-16 17:03:58 +0530

    Full href path

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
 
   return (
     <div className="App" style={{ display: "flex", flexDirection: "column", backgroundImage:`url(${image})`, backgroundSize:"cover" }}>
-      <Router basename='My/'>
+      <Router>
         <NavBar />
         <Routes>
-          <Route path="#/" element={<Home />} />
-          <Route path='#/education' element={<Education />} />
-          <Route path='#/experience' element={<Experience />} />
-          <Route path='#/contact' element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path='/education' element={<Education />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router >
     </div >
