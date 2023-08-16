f2d32df793b1375890e2f580246455bfbc86fd11
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-08-16 16:46:24 +0530

    Changed Route Settings

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/My" element={<Home />} />
-          <Route path='/My/education' element={<Education />} />
-          <Route path='/My/experience' element={<Experience />} />
-          <Route path='/My/contact' element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path='/education' element={<Education />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router >
     </div >
