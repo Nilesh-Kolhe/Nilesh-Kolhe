b8c2e5819c9447f8618d295f9bd0b2f5a5ba622d
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-08-17 13:05:20 +0530

    Updated Education Page

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,9 @@
 const Education = () => {
     return (
-        <h1 style={{marginTop: "5px"}}>Education Page</h1>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "520px" }}>
+            <h1 style={{ marginTop: "5px" }}>Education Page</h1>
+        </div>
     );
 }
- 
+
 export default Education;
