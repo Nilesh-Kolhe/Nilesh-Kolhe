79a057c1bb6a719e9c27d3a145748aaa5bfdc916
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-08-17 16:09:21 +0530

    Changed Height to 100%

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div style={{display: "flex", flexDirection: "column", justifyContent:"center", alignItems: "center", height:"520px"}}>
+        <div style={{display: "flex", flexDirection: "column", justifyContent:"center", alignItems: "center", height:"100%"}}>
             <p className="text-muted" style={{fontSize: "x-large", fontWeight: "600", marginBottom: "0px"}}>Hi There &#128075; ! This is</p>
             <p style={{fontSize: "xxx-large", fontWeight: "700", marginBottom: "2px"}}>Neelesh Kolhe</p>
             <p style={{fontSize: "small", fontWeight: "700"}}>Coder &#128187; | Snapper &#128248; | Footballer &#9917;</p>
