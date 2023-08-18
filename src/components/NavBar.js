eff14a723a4b8b1b2fa1ad705e39c6c5fde1ef12
Nilesh Kolhe <nkolhe@vexcelco.com>
2023-08-18 21:12:05 +0530

    Added Menu Highlighter

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -42,7 +42,7 @@ const NavBar = () => {
                                 <a className={current.includes("education") ? "nav-link current" : "nav-link"} onClick={() => setCurrent("education")} style={{ color: "black", fontWeight: "700", paddingRight: "7px" }} href={`${baseUrl}/education`}>Education</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" onClick={() => setCurrent("work")} style={{ color: "black", fontWeight: "700", paddingRight: "7px" }} href={`${baseUrl}/work`}>Work</a>
+                                <a className={current.includes("work") ? "nav-link current" : "nav-link"} onClick={() => setCurrent("work")} style={{ color: "black", fontWeight: "700", paddingRight: "7px" }} href={`${baseUrl}/work`}>Work</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link disabled" onClick={() => setCurrent("contact")} style={{ color: "black", fontWeight: "700", paddingRight: "7px" }} href={`${baseUrl}/contact`}>Contact Me</a>
