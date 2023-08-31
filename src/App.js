edd0bf50f4993a2ed879f13b643f4ab259e5669e
Nilesh Kolhe <nkolhe@vexcelco.com>
2023-08-31 20:17:40 +0530

    Debug Zero

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import Education from './components/Education';
 import Work from './components/Work';
 import image from "./img2.jpg";
 import telephone from "./telephone.jpg";
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useEffect } from "react";
 
 function App() {
+  const currentLocation = useLocation();
+  console.log('Use Location Location: ', currentLocation);
   let backgroundImage = image;
   useEffect(() => {
-    console.log("Location: ", window.location.href, ' Includes : ', window.location.href.includes("contact"));
+    console.log('UseEffect Use Location Location: ', currentLocation);
+    console.log("window Location: ", window.location.href, ' Includes : ', window.location.href.includes("contact"));
     if(window.location.href.includes("contact")) {
       // eslint-disable-next-line react-hooks/exhaustive-deps
       backgroundImage = telephone;
