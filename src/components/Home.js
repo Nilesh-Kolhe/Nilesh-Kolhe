34b937c14f51f27a6e2efbd70a888f7da82102c4
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-09-05 13:49:34 +0530

    Margin Bottom for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="home-container">
             <p className="text-muted" style={{fontSize: "x-large", fontWeight: "600", marginBottom: "0px"}}>Hi There &#128075; ! This is</p>
             <p style={{fontSize: "xxx-large", fontWeight: "700", marginBottom: "2px"}}>Neelesh Kolhe</p>
-            <p style={{fontSize: "small", fontWeight: "700", marginBottom: "20px"}}>Coder &#128187; | Snapper &#128248; | Footballer &#9917;</p>
+            <p style={{fontSize: "small", fontWeight: "700", marginBottom: "10px"}}>Coder &#128187; | Snapper &#128248; | Footballer &#9917;</p>
             <button type="submit" style={{borderRadius: "8px", height: "35px", width: "160px", fontWeight: "lighter", fontSize: "small"}}> <img style={{height: "9px", width: "20px"}} src={`${download}`} alt="Download Icon" /> Download Resume</button>
         </div>
     );
