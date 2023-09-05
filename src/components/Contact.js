440ecd755ebd6ceaef62f959fd728fb13a6bd931
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-09-05 12:57:49 +0530

    Contact Me Icon Size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,28 +26,28 @@ const Contact = () => {
                         <div>
                             <div style={{ display: "flex" }}>
                                 <div className="img-container" style={{ flexDirection: "row", margin: "25px 5px" }}>
-                                    <img src={`${linkedin}`} alt="LinkedIn Logo" />
+                                    <img style={{height: "40px"}} src={`${linkedin}`} alt="LinkedIn Logo" />
                                     <div>
                                         <p className="fw-bolder">LinkedIn</p>
                                         <a className="text-muted" href="https://www.linkedin.com/in/nilesh-kolhe/" rel="noreferrer" target="_blank">/nilesh-kolhe</a>
                                     </div>
                                 </div>
                                 <div className="img-container" style={{ flexDirection: "row", margin: "25px 5px" }}>
-                                    <img src={`${instagram}`} alt="Instagram Logo" />
+                                    <img style={{height: "40px"}} src={`${instagram}`} alt="Instagram Logo" />
                                     <div>
                                         <p className="fw-bolder">Instagram</p>
                                         <a className="text-muted" href="https://instagram.com/neelesh_kolhe/" rel="noreferrer" target="_blank"><p className="text-muted">/neelesh_kolhe</p></a>
                                     </div>
                                 </div>
                                 <div className="img-container" style={{ flexDirection: "row", margin: "25px 5px" }}>
-                                    <img src={`${twitter}`} alt="Twitter Logo" />
+                                    <img style={{height: "40px"}} src={`${twitter}`} alt="Twitter Logo" />
                                     <div>
                                         <p className="fw-bolder">Twitter</p>
                                         <a className="text-muted" href="https://instagram.com/neelesh_kolhe/" rel="noreferrer" target="_blank"><p className="text-muted">/NeeleshKolhe</p></a>
                                     </div>
                                 </div>
                                 <div className="img-container" style={{ flexDirection: "row", margin: "25px 5px" }}>
-                                    <img src={`${email}`} alt="Email Logo" />
+                                    <img style={{height: "40px"}} src={`${email}`} alt="Email Logo" />
                                     <div>
                                         <p className="fw-bolder">GMail</p>
                                         <a className="text-muted" href="mailto:nilesh.work.001@gmail.com" rel="noreferrer" target="_blank"><p className="text-muted">nilesh.work.001</p></a>
