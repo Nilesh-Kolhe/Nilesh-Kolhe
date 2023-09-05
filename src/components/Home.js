37d5d92b316d49caf873984501d521988cdb646a
Nilesh Kolhe <nilesh.work.001@gmail.com>
2023-09-05 15:28:01 +0530

    Added Resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <p className="text-muted" style={{fontSize: "x-large", fontWeight: "600", marginBottom: "0px"}}>Hi There &#128075; ! This is</p>
             <p style={{fontSize: "xxx-large", fontWeight: "700", marginBottom: "2px"}}>Neelesh Kolhe</p>
             <p style={{fontSize: "small", fontWeight: "700", marginBottom: "10px"}}>Coder &#128187; | Snapper &#128248; | Footballer &#9917;</p>
-            <button type="submit" style={{borderRadius: "8px", height: "35px", width: "160px", fontWeight: "lighter", fontSize: "small"}}> <img style={{height: "9px", width: "20px"}} src={`${download}`} alt="Download Icon" /> Download Resume</button>
+            <a href="Resume.pdf" download="Full_Stack_Angular_React_Dot_Net_Dev.pdf"><button type="submit" style={{borderRadius: "8px", height: "35px", width: "160px", fontWeight: "lighter", fontSize: "small"}}> <img style={{height: "9px", width: "20px"}} src={`${download}`} alt="Download Icon" /> Download Resume</button></a>
         </div>
     );
 }
